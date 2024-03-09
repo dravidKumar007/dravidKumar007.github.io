f31f3ebdaedd522cd33f0dbf502d58bd2bd9e7b8
dravidKumar007 <dravidkumar04@gmail.com>
2024-03-09 12:47:52 +0530

    text on skills images

--- database.js
+++ database.js
@@ -1,18 +1,32 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzxzJhGq3K-c-ypgGNKlNn4Ks-3x2mxsveQLCebLgkxPfz_BAPFURPOEm3jlp4LF9EE/exec'
 const form = document.forms['database']
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', e => { 
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
-function mesbut(){
-    var button=document.getElementById('messagebutton');
+  if(   document.getElementById('nametxt') && 
+  document.getElementById('nametxt').value.trim() !== "" &&
+  validateEmail(document.getElementById('Email').value) &&
+  document.getElementById('txtf') &&   document.getElementById('txtf').value.trim() !== "" 
+){
+  
+  var button=document.getElementById('messagebutton');
     button.style.color = 'white';
     button.innerHTML='<span style="border: 2px solid white; border-radius: 50%; color: greenyellow; background-color: white;">&#10003;</span> Submited';
     button.style.backgroundColor=' rgb(138, 231, 0)';
     button.style.pointerEvents=`none`;
-    console.log('check');
+   console.log('check');
+}
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{ console.log('Success!', response)
+   
+  })
+    .catch(error => console.error('Error!', error.message))
+})
+
+ 
+  function validateEmail(email) {
+    // Regular expression for validating email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 
 }
